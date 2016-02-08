93e2b103ff3e42484a3e1e95cf24e923b41870ef
Petra Barancikova <barp@centrum.cz>
2016-02-08 15:17:15 +0100

    checkboxes instead of radio buttons

--- index.php
+++ index.php
@@ -29,36 +29,36 @@
 
 	$reload = false;
 	// setting up the verb session
-	if (isset($_GET['verb'])) {$_SESSION['verb'] = $_GET['verb'];}
+	if (isset($_GET['verb'])) {$verb = $_GET['verb'];}
 	else {
-		$_SESSION['verb'] = "mit"; 
+		$verb = "mit"; 
 		$reload = true;
 	}
 	// setting up the noun session
-	if  (isset($_GET['noun'])) {$_SESSION['noun'] = $_GET['noun'];}
+	if  (isset($_GET['noun'])) {$noun = $_GET['noun'];}
 	else {
-		$nouns = $verb_to_noun[$_SESSION['verb']]["nouns"]; 
+		$nouns = $verb_to_noun[$verb]["nouns"]; 
 		reset($nouns);
-		$_SESSION['noun'] = $nouns[key($nouns)];	
+		$noun = $nouns[key($nouns)];	
 		$reload = true;
 	}
 	// setting up the frame session
-	if (isset($_GET['frame'])) {$_SESSION['frame'] = $_GET['frame'];}
+	if (isset($_GET['frame'])) {$_SESSION["frame"] = $_GET['frame'];}
 	else {
-		$frames = $verb_to_noun[$_SESSION['verb']]["frames"]; 
+		$frames = $verb_to_noun[$verb]["frames"]; 
 		reset($frames);
-		$_SESSION['frame'] = $frames[key($frames)];
-		$verb_valency = $verb_to_noun[$_SESSION['verb']]["valency"]; 
+		$_SESSION["frame"] = $frames[key($frames)];
+		$verb_valency = $verb_to_noun[$verb]["valency"]; 
 		$reload = true;
 	}
 	if ($reload) {
-		header("Location:index.php?noun={$_SESSION['noun']}&verb={$_SESSION['verb']}&frame={$_SESSION['frame']}");
+		header("Location:index.php?noun={$noun}&verb={$verb}&frame={$_SESSION["frame"]}");
 		die();
 	}
 		
 	//Getting the valency data for noun in PDT-Vallex
-	if (isset($noun_valency[$_SESSION['noun']])) {
-		$noun_data = $noun_valency[$_SESSION['noun']];
+	if (isset($noun_valency[$noun])) {
+		$noun_data = $noun_valency[$noun];
 	} else {
 		$noun_data = Null;
 	}
@@ -73,19 +73,19 @@
 		file_put_contents($filename, json_encode(array()), LOCK_EX) or print("File writing failed.");	 
 	}
 	
-	if (isset($data[$_SESSION['verb']][$_SESSION['noun']])) {
-		$results = $data[$_SESSION['verb']][$_SESSION['noun']];
+	if (isset($data[$verb][$noun])) {
+		$results = $data[$verb][$noun];
 	} else {
 		$results = array();
 	}
 
 	//Changing the frame, i.e. upper part of the screen
-	$frames = $verb_to_noun[$_SESSION['verb']]["frames"]; 
+	$frames = $verb_to_noun[$verb]["frames"]; 
 	//if (isset($_GET['frame'])) {
-		 //$_SESSION['frame'] = $_GET['frame'];
+		 //$frame = $_GET['frame'];
 	//};
-	$verb_valency = $verb_to_noun[$_SESSION['verb']]["valency"]; 
-	$nouns = $verb_to_noun[$_SESSION['verb']]["nouns"];
+	$verb_valency = $verb_to_noun[$verb]["valency"]; 
+	$nouns = $verb_to_noun[$verb]["nouns"];
 	?>
 	 
 	  
@@ -96,21 +96,22 @@
 		<?php include 'sidebar.html'; ?>
 	</div>
 	<div class = "downside">
-		<?php foreach($nouns as $noun) {
-			print "<p><a href='index.php?noun={$noun}&verb={$_SESSION['verb']}&frame={$_SESSION['frame']}'>{$noun}</a></p>";
+		<?php foreach($nouns as $n) {
+			print "<p><a href='index.php?noun={$n}&verb={$verb}&frame={$_SESSION["frame"]}'>{$n}</a></p>";
 			}
 		?>
 	</div>	
 	<div class = "header">  
 		<?php 
 			foreach ($frames as $key => $val) {
-				print "<a href=\"index.php?frame={$val}&noun={$_SESSION['noun']}&verb={$_SESSION['verb']}\">{$key}</a>&nbsp;&nbsp;&nbsp;&nbsp;";
+				print "<a href=\"index.php?frame={$val}&noun={$noun}&verb={$verb}
+				\">{$key}</a>&nbsp;&nbsp;&nbsp;&nbsp;";
 			};
 		?>
 	</div>
 
 	<div class="upper">
-		<object type="text/html" data="<?php echo "http://ufal.mff.cuni.cz/vallex/2.6.3/data/html/generated/lexeme-entries/{$_SESSION['frame']}.html"?>"
+		<object type="text/html" data="<?php echo "http://ufal.mff.cuni.cz/vallex/2.6.3/data/html/generated/lexeme-entries/{$_SESSION["frame"]}.html"?>"
 			style="width:100%;height:100%; margin:0%;" >
 		</object>
 	</div>
@@ -118,9 +119,9 @@
 	<div class="lower">
 
 		<!--lemma-->
-		<h2><?php echo "{$_SESSION['noun']}"; ?></h2><br>
+		<h2><?php echo "{$noun}"; ?></h2><br>
 		<!--choosing the verb valency frame-->
-		<form action="save.php?<?php echo "noun={$_SESSION['noun']}&verb={$_SESSION['verb']}&frame={$_SESSION['frame']}"?>" method="post">
+		<form action="save.php?<?php echo "noun={$noun}&verb={$verb}&frame={$_SESSION["frame"]}"?>" method="post">
 		<p style='text-align: center;'>Valency frame<br><select name="valency">
 			<?php foreach($verb_valency as $val) {
 				print  "  <option value=\"{$val}\"";
@@ -147,11 +148,12 @@
 		</p>
 		
 		<!--valency_frames_of_noun-->
-		<p><?php if (isset($noun_data)) {
+		<p><?php 
 			print '<p style="text-align: center"><b>Noun valency:</b><br></p>';
+			if (isset($noun_data)) {
 			foreach ($noun_data as $key => $val) {
-				print "<input type=\"radio\" name=\"noun_frame\" value=\"{$key}\"";
-				if ($results["noun_frame"] == $key ) {print " checked=\"checked\"";}
+				print "<input type=\"checkbox\" name=\"{$key}_frame\" value=\"{$key}\"";
+				if ($results["{$key}_frame"] == $key ) {print " checked=\"checked\"";}
 				print "/>";
 				print "{$val["elements"]}<br>";
 				print "<div class=\"example\">{$val["examples"]}</div><br>";
@@ -160,7 +162,11 @@
 				print"</textarea><br><br>";
 				
 			}
-		}
+		} else {
+			print "Note: <textarea name=\"noval_note\">";
+			if (array_key_exists("noval_note",$results)) {print_r($results["noval_note"]);} 
+			print"</textarea><br><br>";
+			}
 		?>
 		<!--submit-->
 		<p style='text-align: center;'><input type="submit" value="Send"/></p>
